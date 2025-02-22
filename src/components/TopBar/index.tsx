eebfb571ef4ac37dfd09404547ef120f0455976b
Ivan Leshchenko <ivan.leshchenko@rxdefine.com>
2025-02-22 21:23:49 +0200

    feat: top bar css modules refactor

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,19 +1,20 @@
 import Link from 'next/link';
-
 import Button from '@/components/Button';
+
+import s from './topBar.module.css';
 
 const TopBar = () => {
     return (
-        <div className="bg-white p-4 shadow-md fixed top-0 left-0 w-full z-20">
-            <div className="container mx-auto flex justify-between items-center">
+        <div className={s.topBar}>
+            <div className={s.topBarContainer}>
                 <Link 
                     href="/"
-                    className="text-2xl font-bold font-mono"
+                    className={s.topBarLogo}
                 >
                     FOP-CALC
                 </Link>
 
-                <div className="grid grid-cols-2 gap-4">
+                <div className={s.topBarButtons}>
                     <Button linkTo="/sign-in" variant="secondary">
                         Login
                     </Button>
