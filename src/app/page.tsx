3adac7360c99460df74226eb6e76756933610b05
Ivan Leshchenko <ivan.leshchenko@rxdefine.com>
2025-02-22 21:00:28 +0200

    feat: home css modules refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,31 @@
 import Image from "next/image";
-import './home.css';
+import s from './home.module.css';
 
 export default function Home() {
   return (
-    <div className="hero">
-      <div 
-        className="
-          container m-auto pt-20 justify-between h-full flex flex-col
-          lg:items-center lg:flex-row 
-        "
-      >
-        <div className="flex items-center">
-          <div className="
-            text-center lg:text-left p-5
-            lg:pr-5
-          ">
-            <h1
-              className="text-4xl font-bold text-gray-900"
-            >
+    <div className={s.hero}>
+        <div className={s.heroTextWrapper}>
+          <div>
+            <h1 className={s.heroTitle}>
               FOP calculating tool
             </h1>
-            <p className="text-gray-700 mt-14 text-2xl">
+            <p className={s.description}>
               Are you looking way to easy calculate taxes for your FOP?
-              <br />
               <br />
               You are in!
             </p>
           </div>
         </div>
 
-        <div className="
-          flex items-end justify-center
-          lg:w-1/3 lg:h-full
-        ">
+        <div className={s.heroImageWrapper}>
           <Image
             src="/heroBg.png"
-            alt="Next.js Logo"
+            alt="FOP calculating tool"
             width={300}
             height={500}
-            className="max-h-full lg:w-full lg:object-contain lg:h-auto"
+            className={s.heroImage}
           />
         </div>
-      </div>
     </div>
   );
 }
