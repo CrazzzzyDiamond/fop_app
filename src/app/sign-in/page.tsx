3e8fa3278e248b34e81b499bb642364d2309c716
Ivan Leshchenko <ivan.leshchenko@rxdefine.com>
2025-03-08 23:25:40 +0200

    fix: auth pages fix

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import Paper from '@/components/Paper';
-import { CredentialSignIn, OAuthButtonGroup } from '@stackframe/stack';
 
 const LoginPage = () => {
     return (
         <Paper className="authWrapper">
             <h1 className='title'>Sign In</h1>
-            <OAuthButtonGroup type="sign-in" />
-            <CredentialSignIn />
         </Paper>
     )
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import Paper from '@/components/Paper';
-import { CredentialSignUp, OAuthButtonGroup } from '@stackframe/stack';
 
 const RegisterPage = () => {
     return (
         <Paper className="authWrapper">
             <h1 className='title'>Sign Up</h1>
-            
-            <OAuthButtonGroup type="sign-up" />
-            <CredentialSignUp />
         </Paper>
     )
 }
